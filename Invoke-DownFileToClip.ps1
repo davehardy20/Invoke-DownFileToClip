4da1277c685bb1d9afd09e4cdde8414b4242fa28
davehardy20 <davehardy20@gmail.com>
2017-01-19 10:37:55 +0000

    Added proxy support

--- Invoke-DownFileToClip.ps1
+++ Invoke-DownFileToClip.ps1
@@ -5,36 +5,70 @@
             Retrieve a file from a remote Server and place in clipboard
 
             .Description
-            This cmdlet will take a URL of a file, download the file from the remote server and place the file in the clipboard for future use, without touching disk.
+            This cmdlet will take a URL of a file, download the file from the remote server and place the file in the clipboard for future use, without touching disk. The cmdlet also
+            Provides support for proxies both default and custom.
 
-            .Parameter url
-            URL of the remote file
+            .Parameter remoteurl
+            URL of the remote file.
+
+            .Parameter defaultproxy
+            Use the default system configurted proxy, this is a switch parameter and does not require a value to be set.
+
+            .Parameter customproxy
+            Provide the cmdlet with a custom proxy ie http://192.168.0.1:8080.
+
+            .Parameter username
+            Provide the username for authentication to the custom proxy.
+
+            .Parameter password
+            Provide the assocaiated password for the custom proxy.
 
             Written by Dave Hardy, davehardy20@gmail.com @davehardy20
 
-            Version 0.1
+            Version 0.2
 
             .Example
-            PS> Invoke-DownFileToClip -url http://127.0.0.1:8000/payload.bat
+            PS> Invoke-DownFileToClip -defaultproxy -remoteurl http://somedomain.com/payload.bat
+
+            .Example
+            PS> Invoke-DownFileToClip -customproxy http://192.168.0.1:8080 -username dave -password password -remoteurl http://somedomain.com/payload.bat
+
     #>
 
-    [cmdletbinding()]
+    [cmdletbinding(DefaultParameterSetName="set2")]
     Param
     (
-        [Parameter(Mandatory = $true,
-        HelpMessage = 'Remote Server Url')]
-        [ValidateNotNullorEmpty()]
-        [string]$url
+        #Parameter set for custom proxy
+        [Parameter(ParameterSetName='set1')] $customproxyurl,
+        [Parameter(ParameterSetName='set1', Mandatory=$true)] $username,
+        [Parameter(ParameterSetName='set1', Mandatory=$true)] $password,
+
+        #Parameter set for default system proxy
+        [Parameter(ParameterSetName='set2')][Switch] $defaultproxy,
+        #Mandatory 
+        [Parameter(Mandatory=$true, Position = 0)] $remoteurl
+
     )
-
-    $Command = '' #This is the variable to store the downloaded payload
-
     #webclient variables
     $web = New-Object -TypeName System.Net.WebClient
     $web.UseDefaultCredentials = $true
-    $Command = $web.DownloadString($url)
+    $Command = '' #This is the variable to store the downloaded file
 
-    #Put the download file to the clipboard.
+    if ($PSCmdlet.ParameterSetName -eq 'set2')
+    {
+    (New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+    }
+    ElseIf ($PSCmdlet.ParameterSetName -eq 'set1')
+    {
+    $PSS = ConvertTo-SecureString $password -AsPlainText -Force
+    $SecureCreds = new-object system.management.automation.PSCredential $username,$PSS 
+    $web.Proxy($customproxyurl)
+    $web.Proxy.Credentials($SecureCreds)
+    }
+
+    #Download the file
+    $Command = $web.DownloadString($remoteurl)
+    #Put the download file onto the clipboard.
     $null = [Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
     [Windows.Forms.Clipboard]::SetText($Command)
 }
